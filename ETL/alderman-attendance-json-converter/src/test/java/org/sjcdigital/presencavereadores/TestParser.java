09268fc5b6490b3912aba93fc17241bab25881d9
William Antonio <william.fatecsjc@gmail.com>
2017-09-03 21:24:25 -0300

    Adding test for the PDF parser

--- ETL/alderman-attendance-json-converter/src/test/java/org/sjcdigital/presencavereadores/TestParser.java
+++ ETL/alderman-attendance-json-converter/src/test/java/org/sjcdigital/presencavereadores/TestParser.java
@@ -1,10 +1,14 @@
 package org.sjcdigital.presencavereadores;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.Test;
+import org.sjcdigital.aldermanattendance.model.AldermanAttendance;
 import org.sjcdigital.aldermanattendance.parser.AttendanceParser;
 import org.sjcdigital.aldermanattendance.parser.PDFAttendanceParser;
 import org.sjcdigital.aldermanattendance.parser.XLSAttendanceParser;
@@ -17,6 +21,18 @@ public class TestParser {
 		AttendanceParser xls = AttendanceParser.getParserForType(Paths.get("something.xls"));
 		assertTrue(pdf instanceof PDFAttendanceParser);
 		assertTrue(xls instanceof XLSAttendanceParser);
+	}
+	
+	@Test
+	public void testPdfParser() throws URISyntaxException {
+		Path path = Paths.get(TestParser.class.getResource("/test.pdf").toURI());
+		AldermanAttendance parse = AttendanceParser.getParserForType(path).parse(path);
+		assertEquals("17ª Legislatura", parse.getLegislature());
+		assertEquals("17ª Sessão Ordinária", parse.getSession());
+		assertEquals("04 de Abril de 2017", parse.getDate());
+		assertEquals(21, parse.getAttendance().size());
+		assertEquals("NÃO", parse.getAttendance().get("AMÉLIA NAOMI"));
+		assertEquals("SIM", parse.getAttendance().get("CYBORG"));
 	}
 	
 }
